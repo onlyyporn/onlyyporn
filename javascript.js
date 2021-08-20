8dfe9dec7602b8a55a66871391ecc8d0a80e1c22
onlyyporn <89278552+onlyyporn@users.noreply.github.com>
2021-08-21 00:03:00 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const MINAGE = 18;
+let age = prompt('how old are you?');
+if (age < MINAGE) {
+    alert(`get out.`); 
+    window.location.href = "https://www.youtube.com/watch?v=jWnAC9kkED0";
+}
+ 
